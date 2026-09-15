186de87bcc95b071664ab6afacc90dff1e09cf78
unknown <awoderuoseyi@gmail.com>
2026-09-15 10:00:49 +0100

    update 15_9_26 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import ProductViewer from './components/ProductViewer'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       {/* Other components go here */}
+      <Hero/>
+      <ProductViewer />
     </div>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,23 +1,25 @@
-import React from 'react'
+import {useEffect, useRef} from "react";
 
 const Hero = () => {
-  return (
-    <section>
-      {/* Hero content goes here */}
+    const videoRef = useRef();
 
-      <div>
-        <h1>MacBook Pro</h1>
-        <img src="path/to/title.png" alt="MacBook Title" />
-      </div>
+    useEffect(() => {
+        if(videoRef.current) videoRef.current.playbackRate = 2;
+    }, []);
 
+    return (
+        <section id="hero">
+            <div>
+                <h1>MacBook Pro</h1>
+                <img src="/title.png" alt="MacBook Title" />
+            </div>
 
-      <video/>
-    </section>
-  )
+            <video ref={videoRef} src="/videos/hero.mp4" autoPlay muted playsInline />
+
+            <button>Buy</button>
+
+            <p>From $1599 or $133/mo for 12 months</p>
+        </section>
+    )
 }
-
 export default Hero
-
-
-
-// ---rfce
