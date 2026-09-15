fa3997a073c8b69c9601e33b4a2ffd4f87b8001d
unknown <awoderuoseyi@gmail.com>
2026-09-15 03:08:57 +0100

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section>
+      {/* Hero content goes here */}
+
+      <div>
+        <h1>MacBook Pro</h1>
+        <img src="path/to/title.png" alt="MacBook Title" />
+      </div>
+
+
+      <video/>
+    </section>
+  )
+}
+
+export default Hero
+
+
+
+// ---rfce
