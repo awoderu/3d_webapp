4590d78ae6d15b9911d0791a38a9e8733ffff7fa
unknown <awoderuoseyi@gmail.com>
2026-09-16 10:55:45 +0100

    showcase section update

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import ProductViewer from './components/ProductViewer'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import Showcase from './components/Showcase'
+
+gsap.registerPlugin(ScrollTrigger)
 
 const App = () => {
   return (
@@ -10,6 +15,7 @@ const App = () => {
       {/* Other components go here */}
       <Hero/>
       <ProductViewer />
+      <Showcase />
     </div>
   )
 }
